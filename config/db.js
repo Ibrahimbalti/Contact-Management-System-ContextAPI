449d90076033afb6a18e0c795785a639a3184c44
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-09-28 14:49:25 +0500

    db update
    promiss method convert to async methods

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoUrl');
 
-const connectDB = () => {
-  mongoose
-    .connect(db)
-    .then(() => console.log('connect to Mongodb'))
-    .catch((error) => {
-      console.log(error.message);
-      process.exit(1);
-    });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log('connect to Mongodb');
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
