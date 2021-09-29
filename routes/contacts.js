69a9ca3b9460450a927878cf9be7e8ea96bf1f58
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-09-29 10:35:13 +0500

    get all user contact
    Each user get his contact privately ...Other user does not access your contact details

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const contact = require('../models/Contact');
+const auth = require('../middlewares/auth');
+const user = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Get all contacts ');
+router.get('/', auth, async (req, res) => {
+  try {
+    // find the current login user by id and sort the data by latest data -1 means latest data contact should appear top
+    const contacts = await contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+
+    c;
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
+  }
 });
 
 router.post('/', (req, res) => {
