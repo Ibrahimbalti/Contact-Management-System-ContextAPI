92ed6c2be64dd678821aee4205465796518247fa
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-10-18 00:21:53 +0500

    update code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// const path = require('path');
 const path = require('path');
 const app = express();
 const connectDB = require('./config/db');
@@ -15,13 +16,22 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contact', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-// serve static files on production server
-if (process.env.NODE.ENV === 'production') {
-  //set static folder
+// // serve static files on production server
+// if (process.env.NODE.ENV === 'production') {
+//   //set static folder
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   );
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+
 const PORT = process.env.Port || 5000;
 app.listen(PORT, () => console.log(`server start on PORT  ${PORT}`));
